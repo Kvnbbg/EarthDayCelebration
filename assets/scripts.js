51bd2094bedfd38c45a28d49175a2c58fd95ae4d
Kvnbbg <kvnbbg@gmail.com>
2024-05-01 16:20:05 +0000

    <img             src="/assets/pictures/pacman.gif"             alt="Pacman"             class="w-full h-full"           />           <div class="justify-center">             <div id="shim"></div>             <div id="pacman"></div>             <script src="/assets/pacman.js"></script>           </div>

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,4 +1,28 @@
 function toggleDropdown() {
-    const dropdown = document.getElementById('pacman-dropdown');
-    dropdown.classList.toggle('hidden');
+  const dropdown = document.getElementById("pacman-dropdown");
+  dropdown.classList.toggle("hidden");
+
+  const userResponse = confirm("Do you want to try the rain simulator?");
+  if (userResponse) {
+    window.open("https://kvnbbg.github.io/DynamicRainSimulator2.0/", "_blank");
+  } else {
+    const reason = prompt("Why don't you want to try the rain simulator?");
+    // Store the response in a JSON object
+    const responseInfo = {
+      hour: new Date().getHours(),
+      from: "User",
+      answer: "No",
+      response: reason,
+    };
+    // Convert the object into a string
+    const responseJson = JSON.stringify(responseInfo);
+    // Store the string in local storage
+    localStorage.setItem("userResponse", responseJson);
+  }
+
+  if (!secondResponse) {
+    const secondResponse = confirm(
+      "Are you sure you don't want to try the new feature?",
+    );
+  }
 }
